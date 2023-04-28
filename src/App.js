f20384f2034d442420f88f5190643302ab40ef7c
iss211 <Glen.Abraham@immcossi.com>
2023-04-28 16:33:55 +0530

    button element complete

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import {GoBell,GoCloudDownload,GoDatabase} from "react-icons/go"
 import Button from "./components/Button";
 
 function App(){
+    const handleClick = ()=>{
+        console.log('clicked')
+    }
     return <div>
-        <div><Button primary><GoBell/>Click</Button></div>
+        <div><Button primary onClick={handleClick}><GoBell/>Click</Button></div>
         <div><Button secondary rounded><GoCloudDownload/>Click</Button></div>
         <div><Button warning outline><GoDatabase/>Click</Button></div>
         <div><Button danger>Click</Button></div>
--- src/components/Button.js
+++ src/components/Button.js
@@ -8,6 +8,7 @@ function Button({
   outline,
   rounded,
   children,
+  ...rest
 }) {
   const classes = className('flex items-center px-3 py-1.5 border',{
       'border-blue-500 bg-blue-500 text-white':primary,
@@ -39,7 +40,7 @@ function Button({
     },
   };
 
-  return <button className={classes}>{children}</button>;
+  return <button {...rest} className={classes}>{children}</button>;
 }
 
 export default Button;
