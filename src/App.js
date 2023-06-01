3599eb8b772ae37a0b73308daca544998d20f13f
iss211 <Glen.Abraham@immcossi.com>
2023-06-01 17:58:35 +0530

    drop down component

--- src/App.js
+++ src/App.js
@@ -1,49 +1,21 @@
-import { GoBell, GoCloudDownload, GoDatabase } from 'react-icons/go';
-import Button from './components/Button';
+import { useState } from "react";
+import Dropdown from "./components/Dropdown";
+
 
 function App() {
-  const handleClick = () => {
-    console.log('Clicked!');
-  };
 
-  return (
-    <div>
-      <div>
-        <Button
-          secondary
-          outline
-          rounded
-          className="mb-5"
-          onClick={handleClick}
-        >
-          <GoBell />
-          Click me!!
-        </Button>
-      </div>
-      <div>
-        <Button danger outline>
-          <GoCloudDownload />
-          Buy Now!
-        </Button>
-      </div>
-      <div>
-        <Button warning>
-          <GoDatabase />
-          See Deal!
-        </Button>
-      </div>
-      <div>
-        <Button secondary outline>
-          Hide Ads!
-        </Button>
-      </div>
-      <div>
-        <Button primary rounded>
-          Something!
-        </Button>
-      </div>
-    </div>
-  );
+  const [selection,setSelection] = useState(null);
+
+  const handleSelect = (option)=>{
+    setSelection(option);
+  }
+
+  const options = [
+    {label:'Red',value:'red'},
+    {label:'Green',value:'green'},
+    {label:'Blue',value:'blue'},
+  ]
+  return <Dropdown options={options} value={selection} onChange={handleSelect}/>
 }
 
 export default App;
